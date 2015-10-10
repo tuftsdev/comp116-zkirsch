54a49af40d7aa58282cf196d162b9d3af9446ade
Zach Kirsch <zachkirsch@gmail.com>
2015-10-10 01:51:38 -0400

    adding support for web log

--- alarm/alarm.rb
+++ alarm/alarm.rb
@@ -1,5 +1,41 @@
+#############################################################################
+#                         Comp 116 | HW 2 | Alarm                           # 
+#                        Zach Kirsch, October 2015                          # 
+#                                                                           #      
+# This is a program that acts as an alarm for nmap scans and other          #
+#   vulnerabilities.                                                        #
+#                                                                           #
+#               Usage: ruby alarm.rb [-r <web server log>]                  #
+#                                                                           #
+# If run without the -r option, this will analyze TCP packets in real time, #
+# sounding the alarm if any of the following are detected:                  #
+#  - Nikto scan                                                             #
+#  - X-MAS scan                                                             #
+#  - NULL scan                                                              #
+#  - FIN scan                                                               #
+#  - Any other nmap scan                                                    #
+#                                                                           #
+# If run with the -r option, this will read in a web server log and sound   #
+# the alarm if any of the following are detected:                           #
+#  - NMAP scan (of any variety)                                             #
+#  - Nikto scan                                                             #
+#  - Someone running Rob Graham's Masscan                                   #
+#  - Someone scanning for Shellshock vulnerability                          #
+#  - Anything pertaining to phpMyAdmin                                      #
+#  - Anything that looks like shellcode                                     #
+#                                                                           #
+# TCP attack definitions are based on:                                      #
+#   https://nmap.org/book/man-port-scanning-techniques.html                 #
+#############################################################################
+
 require 'packetfu'
 
+                         ###########################
+			 # Functions for detecting #
+			 #        TCP Scans        #
+			 ###########################
+
+# Detects X-MAS scan - if urg, psh, and fin are all lit
 def is_xmas_scan(pkt)
 	if ! pkt.proto.include?("TCP")
 		return false
@@ -20,6 +56,7 @@ def is_xmas_scan(pkt)
 	return true
 end
 
+# Detects NULL scan - if all the bits are 0, it's a NULL scan
 def is_null_scan(pkt)
 	if ! pkt.proto.include?("TCP")
 		return false
@@ -52,7 +89,7 @@ def is_null_scan(pkt)
 	return true
 end
 
-# only fin flag set
+# Detects FIN scan - only fin flag set
 def is_fin_scan(pkt)
 	if ! pkt.proto.include?("TCP")
 		return false
@@ -85,6 +122,8 @@ def is_fin_scan(pkt)
 	return true
 end
 
+# Detects nmap scan - if nmap is anywhere in the payload, then
+# it's probably an nmap scan
 def is_nmap_scan(pkt)
 	if pkt.payload.include?("nmap")
 		return true
@@ -95,9 +134,18 @@ end
 
 def is_nikto_scan(pkt)
 	return true
+	# TODO
 end
 
-# credit card regex: http://regular-expressions.mobi/creditcard.html
+
+
+                         ##########################
+			 # Function for detecting #
+			 #      credit cards      #
+			 ##########################
+
+# detects credit card by searching for regexp's.
+# (thanks to http://regular-expressions.mobi/creditcard.html for the regex)
 def is_credit_card_leak(pkt)
 	body = {pkt.payload}
 	
@@ -128,11 +176,45 @@ def is_credit_card_leak(pkt)
 	return false
 end
 
+
+
+
+                         ##########################
+			 # Function for analyzing #
+			 #         web logs       #
+			 ##########################
+
+
+def is_masscan(pkt)
+	return true
+	# TODO
+end
+
+def is_shellshock_search(pkt)
+	return true
+	# TODO
+end
+
+def is_phpmyadmin_search(pkt)
+	return true
+	# TODO
+end
+
+def is_shellcode(pkt)
+	return true
+	# TODO
+end
+
+                         ##########################
+			 #   Incident Reporting   #
+			 ##########################
+
+
 def print_incident(pkt, incident, inc_num)
 	puts "#{inc_num}. ALERT: #{incident} is detected from #{pkt.ip_saddr} (#{pkt.proto.last}) (#{pkt.payload})!\n"
 end
 
-def report_incidents(pkt, inc_num)
+def report_live_incidents(pkt, inc_num)
 	orig_inc_num = inc_num
 
 	if is_xmas_scan(pkt)
@@ -166,18 +248,53 @@ def report_incidents(pkt, inc_num)
 	end
 
 	return inc_num
-		
 end
 
+def report_log_incidents(pkt, inc_num)
+	if is_nmap_scan(pkt)
+		print_incident(pkt, "Nmap Scan", inc_num)
+		inc_num += 1
+	end
+
+	if is_nikto_scan(pkt)
+		print_incident(pkt, "Nikto Scan", inc_num)
+		inc_num += 1
+	end
+	
+	if is_masscan(pkt)
+		print_incident(pkt, "Rob Graham's Masscan", inc_num)
+		inc_num += 1
+	end
+
+	if is_shellshock_search(pkt)
+		print_incident(pkt, "Shellshock Vulnerability Search", inc_num)
+		inc_num += 1
+	end
+
+	if is_phpmyadmin_search(pkt)
+		print_incident(pkt, "phpMyAdmin Vulnerability Search", inc_num)
+		inc_num += 1
+	end
+
+	if is_shellcode(pkt)
+		print_incident(pkt, "Shellcode", inc_num)
+		inc_num += 1
+	end
+
+	return inc_num
+end
+
+
+                         ###########################
+			 #      Main Program       #
+			 ###########################
+puts ARGV
 stream = PacketFu::Capture.new(:start => true, :iface => 'eth0', :promisc => true)
 inc_num = 0
 stream.stream.each do |p|
 	pkt = PacketFu::Packet.parse(p)
 	
 	if (pkt != nil)
-		inc_num = report_incidents(pkt, inc_num)
-		if pkt.tcp_flags != nil
-			puts "#{pkt.tcp_flags}"
-		end
+		inc_num = report_live_incidents(pkt, inc_num)
 	end
 end
